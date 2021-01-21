76c1c5d70c033d80d0120e3573a9953c52db258d
FRANCESCO <OLIVOTTI>
2021-01-21 10:01:25 +0100

    added vue to header

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,25 @@ var app = new Vue ({
     
     el: "#app",
     data:{
+<<<<<<< Updated upstream
+=======
+          header: [
+            {
+                logo: "./assets/img/avadabarbers-logo-x1.png",
+                shopping: "fas fa-shopping-cart",
+                title: "Barber Shop",
+                subtitle: "The pinnacle of male grooming",
+                button: "learn more",
+                img: "./assets/img/avadabarbers_hero_focalmirror-600x825.png",
+                folder: "fas fa-folder-plus",
+                span_1: "Demos",
+                span_2: "On Sale",
+                dollar: "$",
+                price: "39"
+            }
+        ],
+
+>>>>>>> Stashed changes
         menuDropdown: ["Home", "Foto", "Prezzi", "Chi Siamo"],
         drop: false
     },
